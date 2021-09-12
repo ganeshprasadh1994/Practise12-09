a0c544e2db92b29545c674ab48ac7590a03e4921
Ganesh Prasadh <ganesh.prasadh@yahoo.com>
2021-09-12 11:33:55 +0530

    Add files via upload
    Changes

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function clicked(){
+btn.addEventListener('click',function(){
     fetch('https://dog.ceo/api/breeds/image/random')
-    .then(
-        data => console.log(data)
-    )
-    .catch(function(error){
-        console.log(error);
+    .then(data =>{
+        console.log(data);
+        var img=document.createElement("img");
+        img.src=data.message;
+        document.body.appendChild(img);
     })
-}
+    .catch(err => console.log(error))
+});
